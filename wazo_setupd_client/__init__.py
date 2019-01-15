b2f43137ebe69b6585bf8d79908d553496517e5f
Francois Blackburn <fblackburn@wazo.community>
2019-01-15 10:59:15 -0500

    update SPDX-License-Identifier to GPL-3.0-or-later

--- wazo_setupd_client/__init__.py
+++ wazo_setupd_client/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 The Wazo Authors  (see the AUTHORS file)
-# SPDX-License-Identifier: GPL-3.0+
+# SPDX-License-Identifier: GPL-3.0-or-later
 
 from wazo_setupd_client.client import SetupdClient as Client
